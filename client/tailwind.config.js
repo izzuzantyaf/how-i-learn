d9f1e365919ea929009a18d1b33890a3113c7d6c
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-12-12 12:43:55 +0700

    disable prefix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'],
-  prefix: 'tw-',
+  // prefix: 'tw-',
   theme: {
     extend: {
       // deklarasi warna saya sendiri
