ea19ed6c97fed7022946cf0b303834b329f0d292
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-11-12 08:13:09 +0700

    Update _app.tsx

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
           components: {
             Button: {
               defaultProps: {
-                size: "sm",
+                size: "md",
                 radius: "md",
               },
             },
